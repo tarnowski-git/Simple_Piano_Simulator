51ccdd17eac2e6d8820634077a1d8ee296006d0c
Konrad Tarnowski <k.tarnowski@student.uksw.edu.pl>
2020-01-24 21:12:56 +0100

    Add few class methods
    Separated creating widgets and setup layout.

--- piano.py
+++ piano.py
@@ -4,111 +4,87 @@ from tkinter import Tk, Frame, Label, Entry, Button, StringVar, CENTER, DISABLED
 class Main_Application(Frame):
     """Main class of application."""
 
+    BLACK = [
+        "C#", "D#", "Space", "F#", "G#", "Bb", "Space", "C#1", "D#1",
+    ]
+
+    WHITE = [
+        'C', 'D', 'E', 'F', 'G', 'A', 'B', "C1", "D1", "E1", "F1"
+    ]
+
+    FONT = ("Arial", 18, "bold")
+
     def __init__(self, master):
         super().__init__(master)
         self.master = master
+        self.black_buttons = list(range(len(self.BLACK)))
+        self.white_buttons = list(range(len(self.WHITE)))
+        # variable to show pressed button in text_box
+        self.var_text = StringVar()
+        self.configure_gui()
+        self.create_widgets()
+        self.setup_layout()
+
+    def configure_gui(self):
+        """Setting general configurations of the application"""
         self.master.title("Virtual Piano")
-        # +0+0 starts window from the upper left corner
-        # self.master.geometry("552x70+0+0")
+        self.master.resizable(0, 0)
         self.master.configure(background="white")
 
-        var_text = StringVar()
-
-        # self.title_label = Label(self.master, text="Piano Musical Keys", font=(
-        #     "Arial", 25, "bold"), padx=8, pady=8, bd=4, bg="powder blue", fg="white", justify=CENTER)
-        # self.title_label.grid(row=0, column=0, columnspan=11)
-
+    def create_widgets(self):
+        """Creating the widgets of the application
+        """
+        self.text_box = Entry(self.master, textvariable=self.var_text, font=self.FONT,
+                              bd=4, bg="powder blue", fg="white", justify=CENTER)
+        # set frames which we relate with buttons
         self.black_keys = Frame(self.master)
-        self.black_keys.grid(row=1)
-
-        self.text_box = Entry(self.master, textvariable=var_text, font=(
-            "Arial", 18, "bold"), bd=4, bg="powder blue", fg="white", justify=CENTER)
-        self.text_box.grid(row=0, column=0, columnspan=11)
-
-        self.btn_Cs = Button(self.black_keys, height=6, width=6, bd=4, text="C#", font=(
-            "Arial", 18, "bold"), bg="black", fg="white")
-        self.btn_Cs.grid(row=0, column=0, padx=5, pady=5)
-
-        self.btn_Ds = Button(self.black_keys, height=6, width=6, bd=4, text="D#", font=(
-            "Arial", 18, "bold"), bg="black", fg="white")
-        self.btn_Ds.grid(row=0, column=2, padx=5, pady=5)
-
-        self.btn_Space_1 = Button(self.black_keys, state=DISABLED, width=2, height=6, bd=4, font=(
-            "Arial", 18, "bold"), bg="powder blue", relief=FLAT)
-        self.btn_Space_1.grid(row=0, column=3, padx=0, pady=0)
-
-        self.btn_Fs = Button(self.black_keys, height=6, width=6, bd=4, text="F#", font=(
-            "Arial", 18, "bold"), bg="black", fg="white")
-        self.btn_Fs.grid(row=0, column=4, padx=5, pady=5)
-
-        self.btn_Gs = Button(self.black_keys, height=6, width=6, bd=4, text="G#", font=(
-            "Arial", 18, "bold"), bg="black", fg="white")
-        self.btn_Gs.grid(row=0, column=6, padx=5, pady=5)
-
-        self.btn_Bb = Button(self.black_keys, height=6, width=6, bd=4, text="Bb", font=(
-            "Arial", 18, "bold"), bg="black", fg="white")
-        self.btn_Bb.grid(row=0, column=8, padx=5, pady=5)
-
-        self.btn_Space_2 = Button(self.black_keys, state=DISABLED, width=2, height=6, bd=4, font=(
-            "Arial", 18, "bold"), bg="powder blue", relief=FLAT)
-        self.btn_Space_2.grid(row=0, column=9, padx=5, pady=5)
-
-        self.btn_Cs1 = Button(self.black_keys, height=6, width=6, bd=4, text="C#1", font=(
-            "Arial", 18, "bold"), bg="black", fg="white")
-        self.btn_Cs1.grid(row=0, column=10, padx=5, pady=5)
-
-        self.btn_Ds1 = Button(self.black_keys, height=6, width=6, bd=4, text="D#1", font=(
-            "Arial", 18, "bold"), bg="black", fg="white")
-        self.btn_Ds1.grid(row=0, column=12, padx=5, pady=5)
-
-        # ===============
-
         self.white_keys = Frame(self.master)
+
+        index = 0
+        for key in self.BLACK:
+            # if every key which is Space
+            if key == "Space":
+                # create a empty button
+                self.black_buttons[index] = Button(
+                    self.black_keys, height=6, width=2, bd=4, bg="white", state=DISABLED, relief=FLAT)
+            else:
+                # otherwise create a normal button
+                self.black_buttons[index] = Button(
+                    self.black_keys, height=6, width=6, bd=4, text=key, font=self.FONT, bg="black", fg="white")
+            # go to the next button
+            index += 1
+
+        index = 0
+        for key in self.WHITE:
+            self.white_buttons[index] = Button(
+                self.white_keys, width=6, height=8, bd=4, text=key, font=self.FONT, bg="white", fg="black")
+            # go to the next button
+            index += 1
+
+    def setup_layout(self):
+        """Setup grid system
+        """
+        self.text_box.grid(row=0, column=0, columnspan=11, padx=10, pady=10)
+        # frames grid are related with the window
+        self.black_keys.grid(row=1)
         self.white_keys.grid(row=2)
 
-        self.btn_C = Button(self.white_keys, width=6, height=8, bd=4, text="C", font=(
-            "Arial", 18, "bold"), bg="white", fg="black")
-        self.btn_C.grid(row=0, column=0, padx=5, pady=5)
+        # set local grids in frames widgets
+        var_col = 0
+        for button in self.black_buttons:
+            # if is a Space, make 1 col space between buttons
+            if button["state"] == DISABLED:
+                button.grid(row=0, column=2 * var_col + 1, padx=0, pady=0)
+            else:
+                button.grid(row=0, column=2 * var_col, padx=5, pady=5)
+            # go to the next column
+            var_col += 1
 
-        self.btn_D = Button(self.white_keys, width=6, height=8, bd=4, text="D", font=(
-            "Arial", 18, "bold"), bg="white", fg="black")
-        self.btn_D.grid(row=0, column=1, padx=5, pady=5)
-
-        self.btn_E = Button(self.white_keys, width=6, height=8, bd=4, text="E", font=(
-            "Arial", 18, "bold"), bg="white", fg="black")
-        self.btn_E.grid(row=0, column=2, padx=5, pady=5)
-
-        self.btn_F = Button(self.white_keys, width=6, height=8, bd=4, text="F", font=(
-            "Arial", 18, "bold"), bg="white", fg="black")
-        self.btn_F.grid(row=0, column=3, padx=5, pady=5)
-
-        self.btn_G = Button(self.white_keys, width=6, height=8, bd=4, text="G", font=(
-            "Arial", 18, "bold"), bg="white", fg="black")
-        self.btn_G.grid(row=0, column=4, padx=5, pady=5)
-
-        self.btn_A = Button(self.white_keys, width=6, height=8, bd=4, text="A", font=(
-            "Arial", 18, "bold"), bg="white", fg="black")
-        self.btn_A.grid(row=0, column=5, padx=5, pady=5)
-
-        self.btn_B = Button(self.white_keys, width=6, height=8, bd=4, text="B", font=(
-            "Arial", 18, "bold"), bg="white", fg="black")
-        self.btn_B.grid(row=0, column=6, padx=5, pady=5)
-
-        self.btn_C1 = Button(self.white_keys, width=6, height=8, bd=4, text="C1", font=(
-            "Arial", 18, "bold"), bg="white", fg="black")
-        self.btn_C1.grid(row=0, column=7, padx=5, pady=5)
-
-        self.btn_D1 = Button(self.white_keys, width=6, height=8, bd=4, text="D1", font=(
-            "Arial", 18, "bold"), bg="white", fg="black")
-        self.btn_D1.grid(row=0, column=8, padx=5, pady=5)
-
-        self.btn_E1 = Button(self.white_keys, width=6, height=8, bd=4, text="E1", font=(
-            "Arial", 18, "bold"), bg="white", fg="black")
-        self.btn_E1.grid(row=0, column=9, padx=5, pady=5)
-
-        self.btn_F1 = Button(self.white_keys, width=6, height=8, bd=4, text="F1", font=(
-            "Arial", 18, "bold"), bg="white", fg="black")
-        self.btn_F1.grid(row=0, column=10, padx=5, pady=5)
+        var_col = 0
+        for button in self.white_buttons:
+            button.grid(row=0, column=var_col, padx=5, pady=5)
+            # go to the next column
+            var_col += 1
 
 
 # Start program
