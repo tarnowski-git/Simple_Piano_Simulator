5739b21049b97c25a736b1021d285fb826e9f6cb
Konrad Tarnowski <k.tarnowski@student.uksw.edu.pl>
2020-01-24 22:12:20 +0100

    Bind sound with buttons

--- piano.py
+++ piano.py
@@ -1,4 +1,6 @@
-from tkinter import Tk, Frame, Label, Entry, Button, StringVar, CENTER, DISABLED, FLAT
+from tkinter import Tk, Frame, Entry, Button, StringVar, CENTER, DISABLED, FLAT
+from functools import partial
+import winsound
 
 
 class Main_Application(Frame):
@@ -48,16 +50,22 @@ class Main_Application(Frame):
                 self.black_buttons[index] = Button(
                     self.black_keys, height=6, width=2, bd=4, bg="white", state=DISABLED, relief=FLAT)
             else:
+                # partial takes care of function and argument instead of 'lambda'
+                cmd = partial(self.button_command, key)
+
                 # otherwise create a normal button
                 self.black_buttons[index] = Button(
-                    self.black_keys, height=6, width=6, bd=4, text=key, font=self.FONT, bg="black", fg="white")
+                    self.black_keys, command=cmd, height=6, width=6, bd=4, text=key, font=self.FONT, bg="black", fg="white")
             # go to the next button
             index += 1
 
         index = 0
         for key in self.WHITE:
+            # partial takes care of function and argument instead of 'lambda'
+            cmd = partial(self.button_command, key)
+
             self.white_buttons[index] = Button(
-                self.white_keys, width=6, height=8, bd=4, text=key, font=self.FONT, bg="white", fg="black")
+                self.white_keys, command=cmd, width=6, height=8, bd=4, text=key, font=self.FONT, bg="white", fg="black")
             # go to the next button
             index += 1
 
@@ -85,6 +93,30 @@ class Main_Application(Frame):
             button.grid(row=0, column=var_col, padx=5, pady=5)
             # go to the next column
             var_col += 1
+
+    def button_command(self, button):
+        """Function for detecting pressed buttons."""
+        if button == "C#":
+            self.play_sound("C_s.wav")
+        elif button == "D#":
+            self.play_sound("D_s.wav")
+        elif button == "F#":
+            self.play_sound("F_s.wav")
+        elif button == "G#":
+            self.play_sound("G_s.wav")
+        elif button == "C#1":
+            self.play_sound("C_s1.wav")
+        elif button == "D#1":
+            self.play_sound("D_s1.wav")
+        else:
+            self.play_sound("{}.wav".format(button))
+        # display info to the Text Box
+        self.var_text.set(button)
+
+    def play_sound(self, filename):
+        """Function working only for Windows"""
+        winsound.PlaySound("assets\\music_notes\\{}".format(
+            filename), winsound.SND_ASYNC)
 
 
 # Start program
