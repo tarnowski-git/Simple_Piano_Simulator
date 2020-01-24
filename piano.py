7a724ad8a8bf88ba8f6b389f6c1a1e107eed838a
Konrad Tarnowski <k.tarnowski@student.uksw.edu.pl>
2020-01-24 03:26:25 +0100

    Create simple application class

--- piano.py
+++ piano.py
@@ -1,5 +1,21 @@
 from tkinter import Tk, Frame
 
 
-root = Tk()
-root.mainloop()
+class Main_Application(Frame):
+    """Main class of application."""
+
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        self.master.title("Virtual Piano")
+        # +0+0 starts window from the upper left corner
+        self.master.geometry("352x70+0+0")
+        self.master.configure(background="white")
+
+
+
+# Start program
+if __name__ == '__main__':
+    root = Tk()
+    application = Main_Application(root)
+    application.mainloop()
